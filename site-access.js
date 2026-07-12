8d722521e488cfd279ee7b04b7d0502c261c6852
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-12 19:19:13 +0100

    Add site access loader

---
+++ site-access.js
@@ -0,0 +1,8 @@
+(function(){
+'use strict';
+if(document.getElementById('siteAccessLoader'))return;
+const s=document.createElement('script');
+s.id='siteAccessLoader';
+s.src='user-passwords.js?v=20260712-107';
+document.body.appendChild(s);
+})();
