17c5931232c4da73466a047021c1ec667d3030d6
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-12 12:03:26 +0100

    Bump Inspector Hub to version 97

--- login-version.js
+++ login-version.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const VERSION='96';
+const VERSION='97';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card||document.getElementById('loginVersion'))return;
