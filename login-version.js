e02d99dc41c93f8dd5a7683211916642880efc94
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-12 10:33:36 +0100

    Update login version to 93

--- login-version.js
+++ login-version.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const VERSION='92';
+const VERSION='93';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card||document.getElementById('loginVersion'))return;
