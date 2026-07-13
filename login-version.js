873d78888f70be5562c2796ae9dde9cfd064210c
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-13 18:13:19 +0100

    Update displayed upload date and time

--- login-version.js
+++ login-version.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const VERSION='13.07.26.121';
-const UPLOADED='13/07/2026 18:05';
+const VERSION='13.07.26.122';
+const UPLOADED='13/07/2026 18:11';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card)return;
