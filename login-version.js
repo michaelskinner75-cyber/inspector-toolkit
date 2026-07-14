7f4864c0e5abf9c1c58c87c728cb787149700af8
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-14 09:10:33 +0100

    Update version for card expand buttons

--- login-version.js
+++ login-version.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const VERSION='13.07.26.138';
-const UPLOADED='13/07/2026 22:10';
+const VERSION='14.07.26.139';
+const UPLOADED='14/07/2026 09:15';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card)return;
