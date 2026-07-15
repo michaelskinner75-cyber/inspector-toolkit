8aab6318b39be4848380f41cf21009793be6cb0d
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-15 01:53:27 +0100

    Prevent management summary flashing on login

---
+++ management-prehide.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict';
+const style=document.createElement('style');
+style.id='managementPrehideCss';
+style.textContent='#loginPage #managementLoginBtn{display:none!important}';
+document.head.appendChild(style);
+})();
