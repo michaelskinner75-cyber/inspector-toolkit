7a636649021cb12014c8f25b9ec4fdacbce43b94
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-15 02:28:58 +0100

    Update Inspector Hub version to 16.0

--- login-version.js
+++ login-version.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const VERSION='15.9';
-const UPLOADED='15/07/2026 20:35';
+const VERSION='16.0';
+const UPLOADED='15/07/2026 20:50';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card)return;
