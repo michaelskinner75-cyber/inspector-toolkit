590a224959f1e1f265a2002b6e624542250ed9d7
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-16 15:39:03 +0100

    Update login version to 18.9

--- login-version.js
+++ login-version.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const VERSION='18.8';
-const UPLOADED='16/07/2026 15:05';
+const VERSION='18.9';
+const UPLOADED='16/07/2026 15:20';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card)return;
