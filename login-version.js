69c0e9cbd79b8b30fecbb5093301943eef3bcc41
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-17 07:47:29 +0100

    Bump version for tickets and fares guide

--- login-version.js
+++ login-version.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const VERSION='21.7';
-const UPLOADED='17/07/2026 14:55';
+const VERSION='21.8';
+const UPLOADED='17/07/2026 15:10';
 function addVersion(){
  const card=document.querySelector('#loginPage .loginCard');
  if(!card)return;
