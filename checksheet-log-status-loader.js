70e06b5491aae675b0ad8a6c83174e13ac236c97
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-09-23 08:29:39 +0100

    Bust NSA status cache for TC option

--- checksheet-log-status-loader.js
+++ checksheet-log-status-loader.js
@@ -13,7 +13,7 @@ function inject(){
  if(!d.getElementById('checksheetLogStatusScript')){
   var b=d.createElement('script');
   b.id='checksheetLogStatusScript';
-  b.src='checksheet-log-status.js?v=20260721-227';
+  b.src='checksheet-log-status.js?v=20260923-nsa-tc-1';
   d.body.appendChild(b);
  }
 }
